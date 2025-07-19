907283fb67bba35532d696ecf7a025367b773e6e
MacTener <mactener228@gmail.com>
2025-07-19 21:06:07 +0300

    Первое задание

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+const usdToEur float64 = 0.86
+const usdToRub float64 = 78.52
+
 func main() {
-	fmt.Println("Hello World!")
+
+	var startEur float64 = 50.0
+	usd := startEur / usdToEur
+	rub := usd * usdToRub
+
+	fmt.Print(rub)
+
 }
