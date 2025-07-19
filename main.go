8d293f56730e497c75a42d754dd130e1aacb612c
MacTener <mactener228@gmail.com>
2025-07-19 21:11:44 +0300

    Исправленное дз

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 const usdToEur float64 = 0.86
 const usdToRub float64 = 78.52
+const eurToRub float64 = usdToRub / usdToEur
 
 func main() {
 
 	var startEur float64 = 50.0
-	usd := startEur / usdToEur
-	rub := usd * usdToRub
+	rub := startEur * eurToRub
 
 	fmt.Print(rub)
 
